21f3cac5fee26d5b6c90f6fcca353062fe5a444f
thornscript <thornscript.reliable417@passmail.net>
2025-04-05 15:54:43 +0900

    config(jwt): JWT 인증 필터를 시큐리티의 필터 체인에 추가함

--- src/main/java/dev/poporo/websocketchat/config/SecurityConfig.java
+++ src/main/java/dev/poporo/websocketchat/config/SecurityConfig.java
@@ -1,5 +1,10 @@
 package dev.poporo.websocketchat.config;
 
+import dev.poporo.websocketchat.common.security.jwt.JwtAccessDeniedHandler;
+import dev.poporo.websocketchat.common.security.jwt.JwtAuthTokenFilter;
+import dev.poporo.websocketchat.common.security.jwt.JwtAuthenticationEntryPoint;
+import dev.poporo.websocketchat.common.security.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,10 +14,16 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtTokenProvider jwtTokenProvider;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -24,6 +35,9 @@ public class SecurityConfig {
         return http.authorizeHttpRequests(r -> r.anyRequest().permitAll())
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .exceptionHandling(handler -> handler.authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                        .accessDeniedHandler(jwtAccessDeniedHandler))
+                .addFilterBefore(new JwtAuthTokenFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 }
